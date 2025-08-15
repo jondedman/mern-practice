58b5e5d492f25bb266d5838d8367ea72b2e1eea7
emma <e.ferrari@live.co.uk>
2025-08-15 11:04:20 +0100

    fix backend failing tests

--- api/tests/controllers/posts.test.js
+++ api/tests/controllers/posts.test.js
@@ -26,6 +26,7 @@ let token;
 describe("/posts", () => {
   beforeAll(async () => {
     const user = new User({
+      fullname: "Testy McTest",
       email: "post-test@test.com",
       password: "12345678",
     });
