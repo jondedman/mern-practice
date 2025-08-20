67b75eef3c893553d11ecee7da4d372b6982740b
jondedman <jonathandedman@googlemail.com>
2025-08-20 22:45:37 +0100

    complete user model and tests

--- api/tests/models/comment.test.js
+++ api/tests/models/comment.test.js
@@ -5,6 +5,7 @@ const Comment = require("../../models/comment");
 
 describe("Comment model", () => {
   let testUser;
+  let testPost;
 
   beforeEach(async () => {
     await Comment.deleteMany({});
@@ -14,9 +15,21 @@ describe("Comment model", () => {
     testUser = await new User({
       fullname: "Test User",
       email: "testuser@example.com",
-      password: "password123!" // fixed the field name
-    }).save(); // fixed chaining
+      password: "password123!"
+    }).save();
+
+    testPost = await new Post({
+      message: "test post",
+      author: testUser._id
+    }).save();
+
   });
+
+  afterEach(async () => {
+  await Comment.deleteMany({});
+  await Post.deleteMany({});
+  await User.deleteMany({});
+});
 
   it("has text", () => {
     const comment = new Comment({ text: "some comment", author: testUser._id });
@@ -33,20 +46,24 @@ it("has an associated post", async () => {
   const comment = new Comment({ text: "some comment", author: testUser._id, post_id: post._id });
   expect(comment.post_id.toString()).toEqual(post._id.toString());
 });
-//   it("can list all posts", async () => {
-//     const posts = await Post.find();
-//     expect(posts).toEqual([]);
-//   });
 
-//   it("can save a post", async () => {
-//     const post = new Post({ 
-//       message: "some message",
-//       author: testUser._id
-//     });
+  it("can list all comments", async () => {
+    const comments = await Comment.find();
+    expect(comments).toEqual([]);
+  });
 
-//     await post.save();
-//     const posts = await Post.find();
-//     expect(posts[0].message).toEqual("some message");
-//     expect(posts[0].author.toString()).toEqual(testUser._id.toString());
-//   });
-});
+  it("can save a comment", async () => {
+    const comment = new Comment({ 
+      text: "some message",
+      author: testUser._id,
+      post_id: testPost._id
+    });
+
+    await comment.save();
+    const comments = await Comment.find();
+    expect(comments[0].text).toEqual("some message");
+    expect(comments[0].author.toString()).toEqual(testUser._id.toString());
+    expect(comments[0].post_id.toString()).toEqual(testPost._id.toString());
+  });
+}
+);
