a401283eb2545d5b036407631d97fd13379b594a
Scott Oswald <scottoswald1@gmail.com>
2025-08-20 14:57:10 +0100

    added new toggle like route

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -5,5 +5,6 @@ const PostsController = require("../controllers/posts");
 
 router.get("/", PostsController.getAllPosts);
 router.post("/", PostsController.createPost);
+router.post("/:id/like", PostsController.toggleLike);
 
 module.exports = router;
