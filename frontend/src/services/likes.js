e0912c3fbcaae2c7b95a7aca363bf2d42a3556a5
Scott Oswald <scottoswald1@gmail.com>
2025-08-21 13:34:02 +0100

    create likes service

---
+++ frontend/src/services/likes.js
@@ -0,0 +1,44 @@
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+export async function getLikes(token) {
+  const res = await fetch(`${BACKEND_URL}/likes`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch likes");
+
+  return res.json();
+}
+
+export async function createLike(token, post_id) {
+  const res = await fetch(`${BACKEND_URL}/likes`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ post_id }),
+  });
+
+  if (!res.ok) throw new Error("Failed to like post");
+
+  return res.json();
+}
+
+export async function deleteLike(token, post_id) {
+  const res = await fetch(`${BACKEND_URL}/likes`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ post_id }),
+  });
+
+  if (!res.ok) throw new Error("Failed to unlike post");
+
+  return res.json();
+}
