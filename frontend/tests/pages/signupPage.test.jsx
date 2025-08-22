99c7b2e20a92ed67a7e7b2891f4af4fb6e1027ee
jondedman <jonathandedman@googlemail.com>
2025-08-22 01:30:03 +0100

    fix signup tests by mocking the upload widget

--- frontend/tests/pages/signupPage.test.jsx
+++ frontend/tests/pages/signupPage.test.jsx
@@ -56,6 +56,12 @@ async function completeSignupForm() {
 describe("Signup Page", () => {
   beforeEach(() => {
     vi.resetAllMocks();
+    window.cloudinary = {
+    createUploadWidget: vi.fn(() => ({
+      open: vi.fn(),
+      close: vi.fn(),
+    })),
+  };
   });
 
   test("allows a user to signup", async () => {
